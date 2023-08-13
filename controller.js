daf575e05f51ba6538a5569e90a905dba47e49cd
dafa <dafadhiyaul24@gmail.com>
2023-08-13 19:55:57 +0700

    add user route

--- controller.js
+++ controller.js
@@ -6,3 +6,13 @@ var connection = require("./connection");
 exports.index = function (req, res) {
   response.ok("Hello from the Node JS RESTful side!", res);
 };
+
+exports.getUsers = function (req, res) {
+  connection.query("SELECT * FROM users", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
--- router.js
+++ router.js
@@ -4,4 +4,5 @@ module.exports = function (app) {
   var jsonku = require("./controller");
 
   app.route("/").get(jsonku.index);
+  app.route("/users").get(jsonku.getUsers);
 };
